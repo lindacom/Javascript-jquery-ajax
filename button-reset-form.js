a6c02d7b6d62ea3306d0fd773811d4e50dfb0ccb
lindacom <43495865+lindacom@users.noreply.github.com>
2020-03-15 18:42:33 +0000

    Create button-reset-form.js

---
+++ button-reset-form.js
@@ -0,0 +1,12 @@
+<input type="button" id="billing" onclick="myFunction()" value="Reset">
+
+
+
+<!--reset billingform -->
+
+
+<script>
+function myFunction() {
+  document.getElementById("billing").reset();
+}
+</script>
